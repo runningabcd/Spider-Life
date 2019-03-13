bee73b2e3687573d4aa409b465e6e317dc3efd4e
runningabcd <821907280@qq.com>
2019-03-13 12:35:59 +0800

    Update solutions.py

--- Algorithm/solutions.py
+++ Algorithm/solutions.py
@@ -34,6 +34,36 @@ class Solution(object):
         print(treenode)
         print(right)
         return max(left, right) + 1
+ 
+#Manacher 马拉车回文串
+class Solution4:
+    def longestPalindrome(self, s: str) -> str:
+        s = '#' + '#'.join(s) + '#'
+        maxRight = 0
+        pos = 0
+        RL = [0] * len(s)
+        for i in range(len(s)):
+            # 求 i 已 pos为对称中心的对称点 j
+            j = 2 * pos - i
+            # 判断 i 的位置 a i在maxright左边  b.i在maxright右边
+            if maxRight > i:
+                # 两种情况 根据 i 到 maxRight的距离判断 若是maxRight-i > RL[j] 那么 RL[j] = RL[i]
+                # 若是maxRight-i < RL[j] 也就是说已j为中心的回文串不在已pos文中心的回文串中 那么 RL[i] >= maxRight-i
+                RL[i] = min(RL[j], maxRight - i)
+            else:
+                # 否则的话从1开始
+                RL[i] = 1
+            # 左右拓展，判断是不是回文串，若是回文串，则加1
+            while i - RL[i] >= 0 and i + RL[i] < len(s) and s[i - RL[i]] == s[i + RL[i]]:
+                RL[i] += 1
+            # RL[i] - 1是已i为中心的回文串的半径 若是i+回文串半径超过maxRight 则 更新maxRight 以及pos
+            if i + RL[i] - 1 > maxRight:
+                maxRight = i + RL[i] - 1
+                pos = i
+            if RL[i] == max(RL):
+                max_index = i
+        lenth = max(RL)-1
+        return s[max_index-lenth: max_index+lenth+1].replace('#', '')
 
 
 print(Solution().maxdepth(TreeNode()))
